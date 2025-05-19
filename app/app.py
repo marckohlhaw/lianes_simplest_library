c1f9182f1a2685cc9f3e89b479c8520d4227837f
Birgit Bunjes <birgit.bunjes@gmx.de>
2025-05-19 14:24:31 +0200

    Update app.py
    Show Books eddited

--- app/app.py
+++ app/app.py
@@ -19,15 +19,27 @@ menu = st.sidebar.selectbox("Navigate", [
 
 # Show books
 if menu == "Show Books":
-    st.header("Books Table")
+    st.header("📚 Overview of All Books in the Library")
+
     books_df = get_all_books_status()
 
     if books_df.empty:
         st.info("No books in the library yet.")
     else:
-        styled_df = style_book_status(books_df)
-        st.write("Legend: 🟢 Available | 🔴 Borrowed")
-        st.write(styled_df.to_html(escape=False), unsafe_allow_html=True)
+        books_df.reset_index(drop=True, inplace=True)
+
+        def color_title_by_status(row):
+            if row["status"] == "Available":
+                return f'<span style="color:darkgreen;"><b>{row["title"]}</b></span>'
+            elif row["status"] == "Borrowed":
+                return f'<span style="color:darkred;"><b>{row["title"]}</b></span>'
+            return row["title"]
+
+        books_df["title"] = books_df.apply(color_title_by_status, axis=1)
+        books_df.drop(columns=["status"], inplace=True)
+
+        # index=False takes away column of index
+        st.write(books_df.to_html(index=False, escape=False), unsafe_allow_html=True)
 
 # Add new book
 if menu == "Add New Book":
